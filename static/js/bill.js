1e061d632b95656e169098f4b11cdc5b0c2b85bf
sanfeer-en <sanfi7254@gmail.com>
2023-10-10 13:37:16 +0530

    issueing modal product listing

--- static/js/bill.js
+++ static/js/bill.js
@@ -53,40 +53,6 @@ $(document).ready(function () {
             container.append(content);
         }
 
-        // // Toggle the image visibility when the user clicks the grid view or list view button
-  
-    //       // Initial view state
-    // let isGridView = true;
-
-    // // Function to toggle between grid view and list view
-    // function toggleView() {
-    //     isGridView = !isGridView;
-
-    //     if (isGridView) {
-    //         // Show the image in grid view
-    //         $('.img-product').show();
-    //     } else {
-    //         // Hide the image in list view
-    //         $('.img-product').hide();
-    //     }
-    // }
-
-    // // Bind click events to the view buttons
-    // $('.btn-grid-view').on('click', function () {
-    //     if (!isGridView) {
-    //         toggleView();
-    //         $(this).addClass('active');
-    //         $('.btn-list-view').removeClass('active');
-    //     }
-    // });
-
-    // $('.btn-list-view').on('click', function () {
-    //     if (isGridView) {
-    //         toggleView();
-    //         $(this).addClass('active');
-    //         $('.btn-grid-view').removeClass('active');
-    //     }
-    // });
     }
 });
 
@@ -129,6 +95,45 @@ $(document).ready(function () {
 });
 
 // modal search div....................
+$(document).ready(function () {
+    $('.searchdiv').on('click', function () {
+        const product_name = $(this).data('product-name');
+        console.log(product_name);
+
+        // Make an AJAX request with product_name to fetch stock data
+        $.ajax({
+            url: '/api/category/?search=' + product_name,
+            type: 'GET',
+            dataType: 'json',
+            success: function (data) {
+                // Handle the success response here
+                console.log(data);
+                
+                // You can update the UI with the fetched stock data here
+                // For example, update the content of the #productSearchResults div
+                const productSearchResults = $('#productSearchResults');
+                productSearchResults.empty(); // Clear previous results
+
+                // Iterate through the data and append results to the div
+                $.each(data, function (index, stockItem) {
+                    const resultItem = $('<div class="result-item">');
+                    resultItem.append('<p class="product-name">Product: ' + stockItem.Product + '</p>');
+                    resultItem.append('<p class="quantity">Quantity: ' + stockItem.Quantity + '</p>');
+                    resultItem.append('<p class="selling-amount">Selling Amount: ' + stockItem.Selling_Amount + '</p>');
+
+                    productSearchResults.append(resultItem);
+                });
+            },
+            error: function (error) {
+                alert('error');
+            }
+        });
+    });
+});
+
+
+
+// ... (the rest of your JavaScript code)
 
 const searchDiv = document.getElementById('searchdiv');
 const modal = document.getElementById('modal');
@@ -153,4 +158,5 @@ modal.addEventListener('click', (event) => {
 window.addEventListener('load', () => {
     modal.style.display = 'none';
 });
+
 
