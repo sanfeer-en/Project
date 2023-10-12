7c09e6d2785fcd9057839624f55e71e355526e84
sanfeer-en <sanfi7254@gmail.com>
2023-10-12 13:22:36 +0530

    search filtered

---
+++ static/js/modal.js
@@ -0,0 +1,87 @@
+// modal search div....................
+
+
+
+$(document).ready(function () {
+    // ...
+
+    // Function to display modal products based on the search input
+    function displayModalProducts(data, searchText) {
+        const resultContainerDiv = $('.result-container');
+        resultContainerDiv.empty();
+
+        for (let i = 0; i < data.length; i++) {
+            const productName = data[i].Product.Product_Name;
+            const quantityValue = data[i].Quantity;
+            const price = data[i].Selling_Amount;
+
+            if (productName.toLowerCase().includes(searchText) && data[i].Product.Is_for_sale) {
+                let content = `
+                    <ul>
+                        <li id="categoryName">${productName}</li>
+                        <li id="sellingAmountDetails">Rs: ${price}</li>
+                        <li id="quantityDetails">Quantity: ${quantityValue}</li>
+                    </ul>
+                `;
+
+                resultContainerDiv.append(content);
+            }
+        }
+    }
+
+    // Listen for input changes in the search input field
+    $('#searchInput').on('input', function () {
+        const searchText = $(this).val().toLowerCase();
+        displayModalProducts(data, searchText);
+    });
+
+    // When the user clicks on a search result, call the displayModalProducts() function.
+    $('.searchdiv').on('click', function () {
+        const product_name = $(this).val();
+        console.log(product_name);
+
+        // Make an AJAX request with product_name to fetch stock data.
+        $.ajax({
+            url: '/api/category/?search=' + product_name,
+            type: 'GET',
+            dataType: 'json',
+            success: function (data) {
+                // Display the products in the modal.
+                displayModalProducts(data, product_name);
+            },
+            error: function (error) {
+                alert('error');
+            }
+        });
+    });
+  });
+  
+  
+  
+
+
+// ... (the rest of your JavaScript code)
+
+const searchDiv = document.getElementById('searchdiv');
+const modal = document.getElementById('modal');
+const closeModal = document.getElementById('closeModal');
+
+searchDiv.addEventListener('click', () => {
+    modal.style.display = 'block';
+});
+
+closeModal.addEventListener('click', () => {
+    modal.style.display = 'none';
+});
+
+// Close the modal when clicking outside of it
+modal.addEventListener('click', (event) => {
+    if (event.target === modal) {
+        modal.style.display = 'none';
+    }
+});
+
+// Prevent modal from showing on page load
+window.addEventListener('load', () => {
+    modal.style.display = 'none';
+});
